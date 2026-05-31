5da4097fbaa95856f0d9aec609f97819a2eb9a9d
warden <wardenscove@gmail.com>
2026-05-31 00:09:32 -0700

    Finished issues with building and linking.

---
+++ mathFuncs/build/CMakeFiles/morecmath.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/morecmath.dir/funcs.cpp.o"
+  "CMakeFiles/morecmath.dir/funcs.cpp.o.d"
+  "libmorecmath.a"
+  "libmorecmath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/morecmath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
